a53aef203f9f2654a8d71b05d48ff4efa179c37c
Thomas Flesher <tjflesher1@gmail.com>
2018-05-16 12:37:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Playground
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            string[] compositeString = { "4for", "2is the", "3week 10 challenge assignment", "1This assignment", "5CIS243" };
+            string[] copyString = new string[0];
+
+           Array.Sort(compositeString);
+
+           for (int i = 0; i < compositeString.Length; i++)
+           {
+               Array.Resize(ref copyString, copyString.Length + 1);
+               copyString[copyString.Length - 1] = compositeString[i].Substring(1);
+           }
+            Console.Write(string.Join(" ", copyString));
+            Console.WriteLine();
+        }
+    }
+}
+
+
