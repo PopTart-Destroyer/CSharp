feb6ec941f172cc0a3a68f881a607c887c102473
Thomas Flesher <tjflesher1@gmail.com>
2018-05-15 22:05:08 -0500

    Add files via upload

---
+++ Assignment_9_TFlesher.cs
@@ -0,0 +1,76 @@
+﻿//T. J. Flesher
+//Assignment 9.1
+//CIS243-T301 - Bellevue University
+//*********************************
+//Develop a C# console application that implements two parallel arrays in Main, one to hold double values called item_price and
+//the other to hold strings called item_name. Each array will hold 5 values.  Use an initializer to fill the item_price array 
+//with the values  15.50, 50.99, 25.00, 115.49, 75.25.  Use an initializer to fill the item_name array with the values "Widget", 
+//"Thingy", "Ratchet", "Clanger", "Fracker".  This application will use a method to change the corresponding prices for items 
+//based on a price point and a multiplier which will require double value types.  The inputs for the price and multiplier are 
+//input from the console.
+//Create two static methods, one called changePrices and one called printit.When the changePrices method is called from 
+//Main you should pass the item_price array by reference, the price point and price difference values input from the console to it.
+//The changePrices method should loop through the item price array and check the price point to determine the increase in price 
+//for each price array element.The basic computation is:
+// ** if the current price is less than the price point then set the price equal to the current price plus the current price times
+//    the price multiplier
+//In the printit method print the item name and the corresponding item price to the console as shown in the output example below
+
+using System;
+
+
+namespace Playground
+{
+    class Assignment_9_TFlesher
+    {
+        static void changePrices(ref double[] item_price, double pricePoint, double percent)
+        {
+
+            for (int j = 0; j < item_price.Length; j++)
+            {
+                if (item_price[j] < pricePoint)
+                {
+                    item_price[j] += item_price[j] * percent;
+                }
+            }
+        }
+
+        static void printIt(string[] itemName, double[] itemPrice)
+        {
+            for (int x = 0; x < itemPrice.Length; x++)
+            {
+                Console.WriteLine("The price for item {0} is {1:C}", itemName[x], itemPrice[x]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double[] item_price = { 15.50, 50.99, 25.00, 115.49, 75.25 };
+            string[] item_name = { "Widget", "Thingy", "Ratchet", "Clanger", "Fracker" };
+            double price_point = 0.0;  //price point
+            double percentage_change = 0.0; //percentage price change
+
+            for (int i = 0; i < item_price.Length; i++)
+            {
+                Console.WriteLine("The price for item {0} is {1:C}", item_name[i], item_price[i]);
+            }
+
+            Console.Write("Enter the price cutoff point (eg $15.00): ");
+            while (!double.TryParse(Console.ReadLine(), out price_point))
+            {
+                Console.Write("Invalid input, numeric values only!");
+                Console.Write("Enter the price cutoff point (eg $15.00): ");
+            }
+            Console.Write("Enter the percentage price change (eg 0.25): ");
+            while (!double.TryParse(Console.ReadLine(), out percentage_change))
+            {
+                Console.Write("Invalid input, numeric values only!");
+                Console.Write("Enter the percentage price change (eg 0.25): ");
+            }
+
+            changePrices(ref item_price, price_point, percentage_change);
+            printIt(item_name, item_price);
+
+        }
+    }
+}
---
+++ ExtraCredit_Wk9_TFlesher.cs
@@ -0,0 +1,73 @@
+﻿//T. J. Flesher
+//Assignment 9 Extra Credit
+//CIS243-T301 - Bellevue University
+//*********************************
+//•	Create a console program where you will implement variables that are needed for this program and will implement the
+//   code within Main and any required static methods.
+//•	Create an array that stores 6 area codes as string and a parallel array that will store 6 per-minutes rates as double.
+//•	Use initializers to set the values of each array according to the following table:
+//Area Code   Per Minute Rate
+//      262	    0.07
+//      414	    0.10
+//      608	    0.05
+//      715	    0.16
+//      815	    0.24
+//      920	    0.14
+
+//•	Declare variables in Main for a boolean found, string area code, string minutes.
+//•	call a static method to calculate the total for the minutes and pass to it the two arrays, and the variables found, 
+//  area and minutes  all by reference.
+//•	In the method:
+//o Prompt the user to enter an area code.
+//o Loop through the area array to find a match to the entered area code.
+//o When the code is found in the array ask the user for the length of time for a call in minutes.
+//o Calculate the cost as minutes* rate from the corresponding rate array element.
+//o Display the cost to the console as currency.
+//o Set found to true.
+//•	If when control returns to Main, the area code was not found from Main display "Sorry - no calls allowed to area XXX".
+
+
+using System;
+
+
+namespace Playground
+{
+    class ExtraCredit_Wk9_TFlesher
+    {
+        static void calcTotalMinutes(ref string[] areaCode, ref double[] perMinRate, ref bool found, ref string area_code, ref string minutes)
+        {
+            double cost = 0.0;
+
+            Console.Write("What are code are you calling? ");
+            area_code = Console.ReadLine();
+
+            for (int i = 0; i < areaCode.Length; i++)
+            {
+                if(area_code == areaCode[i])
+                {
+                    Console.Write("How many minutes is your call? ");
+                    minutes = Console.ReadLine();
+                    found = true;
+                    cost = perMinRate[i] * Convert.ToInt32(minutes);
+                    Console.WriteLine("Your phone call to area code {0} cost {1:C} per minute", areaCode[i], perMinRate[i]);
+                    Console.WriteLine("For {0} minutes the total is {1:C}", minutes, cost);
+                }
+            }
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] arrAreaCode = { "262", "414", "608", "715", "815", "920" };
+            double[] arrPerMinuteRate = { 0.07, 0.10, 0.05, 0.16, 0.24, 0.14 };
+            bool found = false;
+            string area_code = string.Empty, minutes= string.Empty;
+            calcTotalMinutes(ref arrAreaCode, ref arrPerMinuteRate, ref found, ref area_code, ref minutes);
+            if (found == false)
+            {
+                Console.WriteLine("Sorry - no calls allowed to area code {0}", area_code);
+            }
+        }
+    }
+}
